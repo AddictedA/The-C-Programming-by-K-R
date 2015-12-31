3724738dd178558b1bcde0adf1b642da19ada68c
AddictedA <AdrianAurelChiriac@gmail.com>
2015-12-31 10:33:47 -0700

    Final code comments

--- Hello, world!/Hello, World!.c
+++ Hello, world!/Hello, World!.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+/* "Hello, Wolrd!.c" - 
+*	Author:  Adrian Chiriac, 31 December 2015
+*/
 
+#include <stdio.h>	// Standard library for c
+
+////////////////////////////////////////////////////
+// main() - The main body of the program
+////////////////////////////////////////////////////
 void main()
 {
-	printf("Hello, World!\n");
+	// Print function from standard library
+	printf("Hello, World!\n"); // Output string to screen. 
 }
