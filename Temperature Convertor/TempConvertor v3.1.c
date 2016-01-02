f87159f701983ae9d54d44113dcfb5e71886cc0b
AddictedA <AdrianAurelChiriac@gmail.com>
2016-01-01 21:58:35 -0700

    Extended to show reversed tables
    Solved exercise 1-5 from K&R

--- Temperature Convertor/TempConvertor v3.1.c
+++ Temperature Convertor/TempConvertor v3.1.c
@@ -17,7 +17,7 @@ int main()
 {	
    float fahr, celsius;	// Variables
 	
-   printf("Conversion chart for F to C temperatures\n\n");	// Headet for table
+   printf("Conversion chart for F to C temperatures\n\n");	// Header for table
 		
    for (fahr = 0; fahr <= 300; fahr += 20){
       printf("%3.0f\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));	// Output value in Fahrenheit and Celsius
@@ -25,9 +25,25 @@ int main()
 	
    printf("\n\n");
 	
-   printf("Conversion chart for C to F temperatures\n\n");	// Headet for table
+   printf("Conversion chart for C to F temperatures\n\n");	// Header for table
 	
    for (celsius = 0; celsius <= 100; celsius += 5){
+      printf("%3.0f\t%6.1f\n", celsius, (9.0 / 5.0 * celsius + 32.0));	// Output value in Celsius and Fahrenheit
+   }	// End for		
+	
+   printf("\n\n");
+   
+   printf("Conversion chart for F to C temperatures\n\n");	// Header for table
+		
+   for (fahr = 300; fahr >= 0; fahr -= 20){
+      printf("%3.0f\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));	// Output value in Fahrenheit and Celsius
+   }	// End for	
+	
+   printf("\n\n");
+	
+   printf("Conversion chart for C to F temperatures\n\n");	// Header for table
+	
+   for (celsius = 100; celsius >= 0; celsius -= 5){
       printf("%3.0f\t%6.1f\n", celsius, (9.0 / 5.0 * celsius + 32.0));	// Output value in Celsius and Fahrenheit
    }	// End for		
 	
